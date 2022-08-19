2c99c6ec7e325cdd07ba3475b2e4f39e978d5246
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-08-19 15:04:26 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -85,5 +85,5 @@ drawCircle(450, 100, 10)
 drawCircle(400, 150, 10)
 drawCircle(440, 140, 40)
 
-drawCircle(350, 150, 10)
-drawCircle(440, 140, 40)
+drawCircle(350, 200, 10)
+drawCircle(390, 190, 40)
