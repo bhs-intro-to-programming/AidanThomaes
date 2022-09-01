0db5eba025891d35b255d05c0655fb0d1e8697f2
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-09-01 15:11:07 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -10,8 +10,8 @@ const drawFrame = (time) => {
   //drawFilledCircle((time / 3) % width, height/2 , time/100000, 'blue');
   //drawFallingTriangle(time / 10, 1000000/time);
   //drawFallingTriangle((time / 10) % width, 1000000/time);
-  drawFallingTriangle(100000/time, time/10)
-  drawFallingTriangle(100000/time, time*5)
+  drawFallingTriangle(100000/time, time/5)
+  drawFallingTriangle(100000/time, time)
 };
 
 // This is a function that we define to make it easier to draw a triangle. You
