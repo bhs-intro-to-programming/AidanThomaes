dd9c845a04e735e946fdaeddff5732a5a8a3cd49
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-09-01 15:13:43 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -11,7 +11,7 @@ const drawFrame = (time) => {
   //drawFallingTriangle(time / 10, 1000000/time);
   //drawFallingTriangle((time / 10) % width, 1000000/time);
   //drawFallingTriangle(100000/time, time/5)
-  drawFallingTriangle(100000/time, time/3)
+  //drawFallingTriangle(100000/time, time/3)
   drawFilledCircle(1000000/time, time/3 % height, 2, 5)
 };
 
