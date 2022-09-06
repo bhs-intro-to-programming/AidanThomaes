f9a1a36636257539c00e98422d4fb9d678e469e3
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-09-06 15:13:42 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -7,4 +7,7 @@ const multiply = (x1,x2) => (x1*x2)
 const divide = (x1,x2) => (x1/x2)
 
 const mod = (x1,x2) => (x1%x2)
+
 const AverageOfTwo = (x1,x2) => ((x1+x2)/2)
+
+const averageOfThree = (x1,x2,x3) => ((x1+x2+x3)/3)
