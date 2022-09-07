6ca74998364fc0bae94cd5d96de6854e64c37236
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-09-07 14:39:35 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -18,9 +18,5 @@ const manhattanDistance = (x1,y1,x2,y2) => {
 return distance(x1,x2) + distance(y1,y2)
 }
 
-const euclideanDistance = (x1,y1,x2,y2) => {
-const distx = (x1,x2)
-euclideanDistance = (Math.sqrt(Math.abs((distx)**2)+Math.abs((y1-y2)**2)))
-
-
-}
+const euclideanDistance = (x1,y1,x2,y2) => 
+euclideanDistance = (Math.sqrt(Math.abs((x1-x2)**2)+Math.abs((y1-y2)**2)))
