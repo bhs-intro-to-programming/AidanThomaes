79a50c8742e276f06d8f0a1a7c2243127ab4db12
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-09-08 15:03:40 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -18,7 +18,7 @@ const drawFrame = (time) => {
   //drawFallingTriangle(Math.sin(time/500)*1000, time/20)
   //drawFallingTriangle(time/50, Math.sin(time/500)*1500)
   //drawFallingTriangle(Math.sin(time/50)*1000,Math.sin(time/500)*2000)
-  drawFallingTriangle(Math.cbrt(time*100000),time)
+  drawFallingTriangle(Math.cbrt(time*10000),time)
 };
 
 // This is a function that we define to make it easier to draw a triangle. You
