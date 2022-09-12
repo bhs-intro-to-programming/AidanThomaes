42020e83b47718b93c382820d57c420d9a5e4b49
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-09-12 15:15:18 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -33,5 +33,5 @@
 // operator works.
 
 const pigLatin = (s) => s.substring((/aeiou/)) + s.substring(0,s.search((/aeiou/))) + 'ay' ;
-const advancedPigLatin = (s) => s.substring(s === 'e' ? 'way' : 'ay')
+const advancedPigLatin 
 
