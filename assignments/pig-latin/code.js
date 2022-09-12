0eacc66ee95401513bbcf8dac91df1526adc017a
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-09-12 15:02:36 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -32,6 +32,6 @@
 // Feel free to play around in the REPL to make sure you understand how this
 // operator works.
 
-const pigLatin = (s) => (s.substring(s.search[/aeiou/])) +s.substring(0,s.search[/aeiou/])
+const pigLatin = (s) => (s.substring(s.search[/aeiou/])) - s.substring(0,s.search[/aeiou/])
 
 
