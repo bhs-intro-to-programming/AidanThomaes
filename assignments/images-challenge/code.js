2af9f62587e56b4aac6d191da5e7252e5919b2d6
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-09-15 15:11:23 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -60,11 +60,4 @@ while (y < 300) {
 }
 */
 
-let x = 0
-let y = 20
-while (x < 300, y < 300) {
-  drawRect (0 + x, 300, 10, 10)
-  drawRect (0 + y, 300, 10, 10, 'blue')
-  x = x + 20
-  y = y + 20
-}
+n = 8
