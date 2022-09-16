0a9fe019d23ac177f643e3b5f92c746e941f0df1
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-09-16 15:08:46 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -63,7 +63,7 @@ while (y < 300) {
 let n = 0
 while (n < 4) {
   drawFilledRect (50 + (100 * n), 100 + (100 * n), 50, 50, 'red');
-  drawFilledRect (50 + (100 * n), 400, 50, 50)
+  drawFilledRect (50 + (100 * n), 400 - ( 100 * n), 50, 50)
   
   //drawFilledRect (100 + (100 * n), 250, 50, 50)
 
