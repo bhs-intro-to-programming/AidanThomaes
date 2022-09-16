4c0259d64cc189070068f5166e143809965961ea
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-09-16 15:32:32 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -63,8 +63,25 @@ while (y < 300) {
 
 let n = 0;
 while (n < 4) {
-  drawFilledRect (50 + 100 * n, 50 + 100 * n, 50, 50)
-  drawFilledRect (50, 50 + 100 * n, 50, 50)
-  drawFilledRect (50 + 100 * n, 50, 50, 50)
-n = n + 1
-}
+  drawFilledRect (50 + (100 * n), 100, 50, 50, 'red');
+  drawFilledRect (100 + (100 * n), 150, 50, 50);
+  drawFilledRect (50 + (100 * n), 200, 50, 50);
+  drawFilledRect (100 + (100 * n), 250, 50, 50);
+  drawFilledRect (50 + (100 * n), 300, 50, 50);
+  drawFilledRect (100 + (100 * n), 350, 50, 50);
+  drawFilledRect (50 + (100 * n), 400, 50, 50);
+  drawFilledRect (100 + (100 * n), 450, 50, 50);
+
+  drawFilledRect (100 + (100 * n), 100, 50, 50, 'blue');
+  drawFilledRect (50 + (100 * n), 150, 50, 50);
+  drawFilledRect (100 + (100 * n), 200, 50, 50,);
+  drawFilledRect (50 + (100 * n), 250, 50, 50);
+  drawFilledRect (100 + (100 * n), 300, 50, 50,);
+  drawFilledRect (50 + (100 * n), 350, 50, 50);
+  drawFilledRect (100 + (100 * n), 400, 50, 50,);
+  drawFilledRect (50 + (100 * n), 450, 50, 50);
+  
+  n = n + 1;
+};
+
+
