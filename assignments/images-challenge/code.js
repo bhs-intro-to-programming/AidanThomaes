38fa9161d75e36b15872286221186b9f0cc943ab
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-09-16 15:22:18 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -76,9 +76,9 @@ while (n < 4) {
   drawFilledRect (100 + (100 * n), 200, 50, 50,);
   drawFilledRect (50 + (100 * n), 250, 50, 50);
   drawFilledRect (100 + (100 * n), 300, 50, 50,);
-
+  drawFilledRect (50 + (100 * n), 350, 50, 50);
   drawFilledRect (100 + (100 * n), 400, 50, 50,);
-  
+  drawFilledRect (50 + (100 * n), 450, 50, 50);
   
 
   n = n + 1;
