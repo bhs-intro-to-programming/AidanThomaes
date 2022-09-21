79823acdace95056e6f5f7fafd370207b8073ec0
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-09-21 14:59:58 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -30,7 +30,7 @@ let radius = 10
 let diameter = radius / 2
 let x = 0;
 while (x < width - diameter) {
-  drawFilledCircle (0 + x, 300, radius, 'red');
+  drawFilledCircle (0 + x, 300, radius + diameter, 'red');
   x = x + radius * 2;
 };
 
