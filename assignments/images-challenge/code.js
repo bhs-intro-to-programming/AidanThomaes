b694c5c8a62f1fc13bab0c1e98391bc49abaab1f
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-09-22 14:50:02 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -39,50 +39,9 @@ let radius = 10
 let diameter = radius * 2
 let x = 0
 while (x < width - radius) {
-  drawFilledCircle (0 + radius + x, height/2, radius, 'red')
-  drawFilledCircle (0 + diameter  + x, height/2, radius, 'blue')
-  
+ 
   x = x + diameter
 }
 
-/*
-let x = 50;
-while (x < 300) {
-  drawCircle (250, 300, 288 - x, 'red', 25);
-  x = x + 50;
-};
 
-let y = 50
-while (y < 300) {
-  drawCircle (250, 300, 263 - y, 'blue', 25);
-  y = y + 50;
-};
-*/
 
-/*
-let n = 0;
-while (n < 4) {
-  drawFilledRect (50 + (100 * n), 100, 50, 50, 'red');
-  drawFilledRect (100 + (100 * n), 150, 50, 50);
-  drawFilledRect (50 + (100 * n), 200, 50, 50);
-  drawFilledRect (100 + (100 * n), 250, 50, 50);
-  drawFilledRect (50 + (100 * n), 300, 50, 50);
-  drawFilledRect (100 + (100 * n), 350, 50, 50);
-  drawFilledRect (50 + (100 * n), 400, 50, 50);
-  drawFilledRect (100 + (100 * n), 450, 50, 50);
-
-  drawFilledRect (50, 100 + 100 * n)
-
-  drawFilledRect (100 + (100 * n), 100, 50, 50, 'blue');
-  drawFilledRect (50 + (100 * n), 150, 50, 50);
-  drawFilledRect (100 + (100 * n), 200, 50, 50,);
-  drawFilledRect (50 + (100 * n), 250, 50, 50);
-  drawFilledRect (100 + (100 * n), 300, 50, 50,);
-  drawFilledRect (50 + (100 * n), 350, 50, 50);
-  drawFilledRect (100 + (100 * n), 400, 50, 50,);
-  drawFilledRect (50 + (100 * n), 450, 50, 50);
-  
-  n = n + 1;
-};
-
-*/
