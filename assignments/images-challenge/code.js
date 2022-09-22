f2926fe7ff25b9e634251f71724b46155d7901d3
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-09-22 15:27:34 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -47,12 +47,12 @@ const drawAltLine = (radius) => {
     x = x + 1;
   }
   let y = 0
-  while (y * diameter + diameter < width) {
+  while (y * diameter + diameter-1 < width) {
     drawFilledCircle (0 + (radius * 3) + (y * diameter * 2), height/2, radius, 'blue')
     y = y + 1
   }
 }
-drawAltLine (12)
+drawAltLine (1)
 
 const drawConcentricCircle = (radius) => {
 
