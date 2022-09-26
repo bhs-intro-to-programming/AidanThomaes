134634211639a7851479c92be87442b4f83cfa59
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-09-26 15:10:46 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -51,7 +51,10 @@ const drawAltLine = (r) => {
 drawAltLine ();
 
 const drawConcentricCircle = (n) => {
-  
+  for (let d = width; d > 0; d = d - n) {
+    drawCircle (width/2, height/2, d - n, 'red')
+  }
+
   for (let d = width - n  ; d > 0; d = d - n) {
     drawCircle (width/2, height/2, d - n, 'blue')
   }
