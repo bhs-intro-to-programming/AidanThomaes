aa770ad8d7eddef82c227c1c18014703baf000cb
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-09-27 15:28:09 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -63,7 +63,7 @@ const pattern = (n) => {
 
 const drawConcentricCircles = (n) => {
   while (Math.floor(n/2) > 0 ) {
-    drawCircle (width/2, height/2,width/2+30-n*30)
+    drawCircle (width/2, height/2,width/2+50-n*50)
     
     n = n - 1
   }
