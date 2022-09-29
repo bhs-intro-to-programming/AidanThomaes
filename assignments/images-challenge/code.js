284ac3021fdf4b8d46b004af327ccaa441c831d6
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-09-29 15:20:12 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -67,7 +67,7 @@ const drawConcentricCircles = (n) => {
   let z = width / n
   while (n < width) {
     drawCircle (width/2, height/2, width/2 - y , 'red')
-    //drawFilledCircle (width/2, height/2, width/2 - n - Math.floor(x/2), 'blue')
+    drawFilledCircle (width/2, height/2, width/2 - y - Math.floor(x/2), 'blue')
     n = n + x
     y = y + z
   }
