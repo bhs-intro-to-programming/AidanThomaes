0d898c40f4651d7e14b27efd8c811a3c4626276a
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-09-30 15:19:50 -0700

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -10,8 +10,25 @@ const hypotenuse = (a, b) => (Math.sqrt(a ** 2 + b ** 2))
 const maxRadius = (w, h) => (Math.min (w, h)/2)
 const numCircles = (r,w) => (w / r)
 
-const canSleepIn = (weekday, vacation) => {
-  weekday = false
-  vacation = true
-  return canSleepIn (!weekday || vacation)
+const canSleepIn = (isWeekday, onVacation) => {
+  isWeekday = false
+  onVacation = true
+  return canSleepIn (!isWeekday || onVacation)
+}
+const canGoToProm = (isSenior, isInvited, isExcluded) => {
+  isSenior = true
+  isInvited = true
+  isExcluded = false
+  return canGoToProm (isSenior || isInvited &&! isExcluded)
+}
+const getsSpeedingTicket = (s, isGrouchy) => {
+  isGrouchy = true
+  
+}
+getsSpeedingTicket()
+const moreThanTwiceAsLong
+const aFartherThanB = (x,y,z) => {
+  a = Math.abs(x-z)
+  b = Math.abs(y-z)
+  Math.min ()
 }
