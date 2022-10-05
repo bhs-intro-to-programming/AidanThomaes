4e0f1b417a2e9188480de3787f5bfeb3b9d02200
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-10-05 14:53:35 -0700

    Updating assignments/number-function-practice/code.js

---
+++ assignments/number-function-practice/code.js
@@ -0,0 +1,9 @@
+const sum = (a,b) => (a+b);
+const difference= (a,b) => (a-b);
+const product = (a,b) => (a*b);
+const ratio = (a,b) => (a/b);
+const remainder = (a,b) => (a%b);
+const average = (a,b) => (a+b)/2;
+const averageOf3 = (a,b,c) => (a+b+c)/3;
+const generalAverage = (total,count) => (total/count);
+const distance = (a,b) => (Math.abs(a-b))
