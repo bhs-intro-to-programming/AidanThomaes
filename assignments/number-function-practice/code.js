ffb8bea659384afcce187a4e050907fb9d4a9f51
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-10-05 14:56:22 -0700

    Updating assignments/number-function-practice/code.js

--- assignments/number-function-practice/code.js
+++ assignments/number-function-practice/code.js
@@ -7,3 +7,4 @@ const average = (a,b) => (a+b)/2;
 const averageOf3 = (a,b,c) => (a+b+c)/3;
 const generalAverage = (total,count) => (total/count);
 const distance = (a,b) => (Math.abs(a-b))
+const manhattan = (x1,y1,x2,y2) => (Math.abs(Math.abs(x1-x2)-Math.abs(y1-y2)))
