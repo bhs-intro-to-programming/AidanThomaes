7fc2230061d150a0075d0e88dd8313c5593f5bfd
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-10-06 14:42:38 -0700

    Updating assignments/number-function-practice/code.js

--- assignments/number-function-practice/code.js
+++ assignments/number-function-practice/code.js
@@ -9,3 +9,6 @@ const generalAverage = (total,count) => (total/count);
 const distance = (a,b) => (Math.abs(a-b));
 const manhattan = (x1,y1,x2,y2) => (Math.abs(x1-x2)+Math.abs(y1-y2));
 const twoDDistance = (x1,y1,x2,y2) => (Math.sqrt(Math.abs(x1-x2)**2+Math.abs(y1-y2)**2))
+
+const perimeterOfSquare = (sideLength) => (4*sideLength)
+const perimeterOfRect = (width,height) => (2*(width+height))
