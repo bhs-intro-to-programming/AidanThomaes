2849c1ef7f80c2f903f7b96c56f0d51727bcd4b9
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-10-17 15:06:04 -0700

    Updating assessments/unit-01-booleans-and-strings/code.js

--- assessments/unit-01-booleans-and-strings/code.js
+++ assessments/unit-01-booleans-and-strings/code.js
@@ -24,9 +24,9 @@ const willSeeTweet = (followTweeter,followRetweeter,blockedTweeter) => {
 
 const evenGreaterThanZero = (x) => {
   if (x>0) {
-    if (x%2 === 0) {
+    
       true
-    }
+    
   }
 }
 
