8ab0851b5362c6199d3e8e5b0624f6c5ae5b15b2
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-10-18 14:48:20 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -2,7 +2,8 @@ const countTens = (numbers) =>{
   if (numbers.length > 0) {
     for (let x = 0; x < numbers.length; x++ ) {  
       if (numbers[x]===10) {
-        return x + 1
+        let numTens = 0
+        return numTens + 1
       } 
     }
   } else {
