134e148befc1cb032645abba302850379382ab56
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-10-21 15:11:05 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -39,7 +39,9 @@ const pyramid = (n) => {
   let list = [];
   for (let i = 0; i < 2; i++) {
     for (let x = 0; x < i; x++) {
-      (list.push(i,x))
+      if (x > 0){
+        (list.push(i,x))
+      }
     }
   }
   return list
