78680e2c0fe8526868df33cac40bb3cb032a36bc
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-10-27 15:23:17 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -35,5 +35,5 @@ const secondHalf = (s) => {
 }
 
 const upDown = (s) => {
-  return s[0].toUpperCase() + s.substring(1).toLowerCase()
+  return s.substring(0).toUpperCase() + s.substring(0).toLowerCase()
 }
