6f51c4b842bde14977d0b3dd04bf9f4d65097866
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-10-27 15:32:41 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -43,5 +43,5 @@ const everyOther = (s) => {
 }
 
 const upDownLastCharacter = (s) => {
-  return s[0].toUpperCase() + s[length-1].toLowerCase()
+  return s[0].toUpperCase() + s.substring(length).toLowerCase()
 }
