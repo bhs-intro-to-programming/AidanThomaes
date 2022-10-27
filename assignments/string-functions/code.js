c418ca7b725b26395bbbfd7ac4c8b1f1454f0959
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-10-27 15:01:37 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -9,3 +9,7 @@ const stringContains = (s1,s2) => {
 const firstAndLast = (s) => {
   return s.substring(0,1) + s.substring(s.length-1,s.length)
 }
+
+const swapFrontAndBack = (s) => {
+  return s.substring(0,Math.floor(s.length/2)) + s.substring(s.length/2)
+}
