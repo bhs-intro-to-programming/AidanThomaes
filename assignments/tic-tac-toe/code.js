60ee7ae84e60f4e3adee881537a452ebbd5b1f23
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-11-02 14:36:10 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -20,7 +20,7 @@ registerOnclick((x, y) => {
 const turn = (x, y) => {
   drawText(player1, x, y, 'black', Math.min(width, height) * 0.3);
   
-    drawText(player2, x, y, 'black', Math.min(width, height) * 0.3);
+  drawText(player2, x, y, 'black', Math.min(width, height) * 0.3);
 
 
 
