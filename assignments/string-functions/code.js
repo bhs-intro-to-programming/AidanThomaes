b54d601f5f0710d85ad2ad39938dbe11441e89e7
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-11-03 15:22:15 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -3,7 +3,7 @@ const findFnord = (s) => {
 }
 
 const stringContains = (s1,s2) => {
-  return s.substring(s.indexOf(s1)) === s2
+  return s1.substring(s.indexOf(s2)) === s2
 }
 
 const firstAndLast = (s) => {
