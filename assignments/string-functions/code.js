245653a6f2bf74547368e5b23fa0567fe3719083
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-11-03 15:33:26 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -28,11 +28,7 @@ const isAllUpperCase = (s) => {
 }
 
 const sameIgnoringCase = (s1,s2) => {
-  if (s1 === s2) {
-    return true
-  } else{
-    return false
-  }
+  return s1.toLowerCase === s2.toLowerCase
 }
 
 const firstHalf = (s) => {
