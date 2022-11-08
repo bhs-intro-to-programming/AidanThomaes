d784e76e7429939644e04810bb80d95d479d48f1
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-11-08 14:48:56 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -22,8 +22,9 @@ const midpoint = (object1, object2) => {
 
 const sumSalaries = (object) => {
   let add = 0
-  for (let i = 0; i < object.length; i++) {
-    
+  for (let i = 0; i < object.length-1; i++) {
+    const o = object[i]
+    add = add + o
   }
   return add
 }
