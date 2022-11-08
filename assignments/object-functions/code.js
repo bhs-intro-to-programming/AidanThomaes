94f9f9cd2a2f768666768457b150961c20a22d88
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-11-08 15:31:01 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -39,9 +39,9 @@ const newHighScore = (currentHighScore, array) => {
 
 const summarizeBooks = (array) => {
   let add = 0
-  let titles = {'titles'}
+  let titles = {'titles':x}
   for (let i = 0; i < array.length; i++) {
-  titles = {'titles':array.length}
+  titles = {'titles':array[i].title}
     add = add + array[i].pages
   }
   return titles
