035042dc5bac387eb1adbdc56fbe8b3dc3e06b0d
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-11-15 14:42:21 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -6,6 +6,12 @@ const area = (rect) => {
 };
 
 const higherPaid = (e1, e2) => {
+  if (e1.salary > e2.salary) {
+    return e1
+  } else {
+    return e2
+  }
+
 };
 
 const isSamePoint = (p1, p2) => {
