3c3e98e15f7953fa9dd6ba4304f561882482ae52
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-11-16 14:52:10 -0800

    Updating assignments/array-functions/code.js

---
+++ assignments/array-functions/code.js
@@ -0,0 +1,27 @@
+const countTens = (array) => {
+  let count = 0
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 10) {
+      count++
+    }
+  }
+  return count
+}
+
+const sum = (array) => {
+  let add = 0
+  for (let i = 0; i < array.length; i++) {
+    add = add + array[i]
+  }
+  return add
+}
+
+const evens = (array) => {
+  let list = []
+  for (let i = 0; i < array.legth; i++) {
+    if (array[i] % 2 === 0) {
+      list.push(array[i])
+    }
+  }
+  return list
+}
