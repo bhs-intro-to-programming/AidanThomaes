9cf97331e3d1d140916e3b851810e71382e7f0af
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-11-16 15:00:34 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -38,7 +38,7 @@ const anyOverOneHundred = (array) => {
 const pyramid = (n) => {
   let list = []
   for (let i = 0; i < n; i++) {
-    for (let x = 0; x < i + 1; x++) {
+    for (let x = 0; x < i; x++) {
       if (x > 0) {
         list.push(i, x)
       } else {
