b4ac27cbc36847c77d9e00b4c4128bef2ae061ec
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-11-18 15:25:50 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -17,3 +17,10 @@ const distance = (object1, object2) => {
 const midpoint = (object1, object2) => {
   return {x : (object1.x+object2.x)/2, y : (object1.y+object2.y)/2}
 }
+
+const sumSalaries = (array) => {
+  let add = 0
+  for (let i = 0; i < array.length; i++) {
+    add += array[i].salary
+    }
+}
