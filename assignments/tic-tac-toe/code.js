1f34248cfa52011b5612ac93da49e8f8aacc4622
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-11-29 15:34:14 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -28,8 +28,7 @@ registerOnclick((x, y) => {
   move++
   
   
-  const row = (y * 9) / height / 9 + 56
-  const column = (y * 3) / width + 88
+
 
   drawText(turn, x, row, 'black', Math.min(width, height) / 3)
 });
