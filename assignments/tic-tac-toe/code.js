e04a4d2009711c51ef9b551835855b670843ccb9
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-11-29 15:06:01 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -26,7 +26,7 @@ let move = 0
 registerOnclick((x, y) => {
   const turn = move % 2 === 0 ? 'X' : 'O';
   move++
-  const row = y / height + y* 10
+  const row = y / height + y + 50
   
   drawText(turn, x, row, 'black', Math.min(width, height) / 3)
 });
