ed2deabeba17772a918fb694abb46b7e6c9558db
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-12-01 14:58:56 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -26,7 +26,7 @@ let move = 0
 registerOnclick((x, y) => {
   if (move % 2 === 0) {
     if (y < height / 3 && x < width / 3) {
-      drawText ('X', 120, 60, 'black' , Math.min(width,height)/3)
+      drawText ('X', 100, 60, 'black' , Math.min(width,height)/3)
     }
   }
   
