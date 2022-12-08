401cf45dd61e70aceba0a98f5e7da7a2f1fc7c00
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-12-08 15:08:42 -0800

    Updating assessments/2022-12-08-control-constructs/code.js

--- assessments/2022-12-08-control-constructs/code.js
+++ assessments/2022-12-08-control-constructs/code.js
@@ -25,6 +25,9 @@ const timesTable = (n) => {
 
 const containsX = (s) => {
   for (let i = 0; i < s.length; i++) {
-    return s[i] === 'x'
+    if (s[i] === 'x'){
+      return true
+    }
   }
+  return false
 }
