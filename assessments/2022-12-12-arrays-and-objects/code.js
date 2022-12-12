166fa29eba0e060fb6f1d8ede709b31ad6befbe2
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-12-12 15:45:00 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -9,7 +9,7 @@ const makeMove = (mark, row, column) => {
 let board = makeBoard
 let move = makeMove('X', 1, 1);
 const placeMark = (board, move) => {
-  board[0][0] = 'x'
+  board[0][2] = 'x'
   return board
 }
 
