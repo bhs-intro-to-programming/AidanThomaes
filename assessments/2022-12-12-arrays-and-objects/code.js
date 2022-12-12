57bc3d22637b19a8d38f99a9f0c6452d5b87cf32
AidanThomaes <111320407+AidanThomaes@users.noreply.github.com>
2022-12-12 15:14:12 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -13,3 +13,11 @@ const placeMark = (makeBoard, makeMove) => {
 const allTheSame = (array) => {
   return array[0] === array[1] && array[1] === array[2]
 }
+const board =    [
+     ['X', '', ''],
+     ['O', '', ''],
+     ['', 'X', '']
+   ]
+const exractColumn = (board, i) => {
+  return [board[i][0], board[i][1], board[i][2]]
+}
